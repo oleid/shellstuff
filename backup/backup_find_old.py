7ff7563e37abf777ceca93e59b6b223321bb0092
Olaf Leidinger <oleid@mescharet.de>
2016-03-25 15:15:27 +0100

    make it actually work

--- backup/backup_find_old.py
+++ backup/backup_find_old.py
@@ -12,19 +12,24 @@ from datetime   import date, datetime
 from bisect     import bisect_left
 from random     import sample
 from sys        import stderr, argv, exit
+from math       import floor, ceil
+
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 DATE_FORMAT = "%Y-%m-%d@%H:%M:%S"
 VERBOSE     = False
 
-# maximum age in days ==> maximal number of backups per month
+# maximum age in days ==> backup frequency (number / days)
 # btrfs doesn't like too many snapshots
-BackupAgeFreqDist = {    62 : 31,
-                        124 : 15,
-                        240 : 10,
-                        365 : 5,
-                    1.5*356 : 2,
-                      2*365 : 1,
-                      3*365 : 1.0/12 }
+BackupAgeFreqDist = {     7 : 1,
+                         15 : 0.75,
+                         30 : 0.5,
+                         60 : 10/30,
+                        124 : 12/365,
+                        355 : 1/300  # from there on...
+                    }
 
 ##############################################################################
 
@@ -41,46 +46,57 @@ def getBackupDates(basedir):
 def getSortedBackupDates(basedir):
     today = datetime.today()
     skeys = sorted(BackupAgeFreqDist.keys())
-    hist  = dict( ( (k, {}) for k in skeys)) # dict of (empty) dicts
-    
+    hist  = dict( ( (k, []) for k in skeys)) # dict of (empty) lists
+    rest  = []
+    oldest_so_far = -1
+
     for x in getBackupDates(basedir):
         delta  = today-x 
         keypos = bisect_left(skeys, delta.days)
-        keypos = len(skeys)-1 if keypos >= len(skeys) else keypos
-        key    = skeys[keypos]
-        
-        # test, if year oder year+month shall be used depending on the given frequency
-        where = x.year*366 + x.month if BackupAgeFreqDist[key] >= 1 else x.year*366
-        # add backup to list of backups for this date
-        if where in hist[key]:
-            hist[key][where].append(x)
+        if keypos >= len(skeys):
+            # special treatment for older entries
+            oldest_so_far = max(delta.days, oldest_so_far)
+            rest.append(x)
         else:
-            hist[key][where] = [ x ]
+            key    = skeys[keypos]
+            hist[key].append(x)
+
+    if len(rest) > 0:
+        # we have old backups, special handling for them
+        oldest_age_configured = sorted(BackupAgeFreqDist.keys())[-1]
+        oldest_age_freq       = BackupAgeFreqDist[oldest_age_configured]
+        BackupAgeFreqDist[oldest_so_far] = oldest_age_freq
+        hist[oldest_so_far] = rest
+
+    if VERBOSE:
+        print("Hist for dir %s:" % basedir)
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(hist)
+
     return hist
 
 ##############################################################################
-
 def findBackupsForDeletion(basedir):
     if VERBOSE: print("Statistics for backup dir %s:" % basedir, file=stderr)
 
-    hist  = getSortedBackupDates(basedir)
-    todel = []
+    hist      = getSortedBackupDates(basedir)
+    todel     = []
+    bins      = sorted(hist.keys())
+    IVs       = map(lambda x,y: y-x, [0]+bins,bins) # calc interval len
 
-    for key in  sorted(BackupAgeFreqDist.keys()):
-        wanted_freq = BackupAgeFreqDist[key]
-        for period in sorted(hist[key]):
-            year  = period / 366
-            month = period % 366
-            freq  = len(hist[key][period]) / (1.0 if wanted_freq >= 1 else 12.0)
-            if VERBOSE: print("%i-%i : %f %f" % (year,month, freq, wanted_freq), file=stderr)
-            
-            if freq > wanted_freq:
-                # we'll have to select N backups which shall be deleted
-                N = round((freq - wanted_freq) * ( 1 if wanted_freq >= 1 else 12))
-                what = hist[key][period]
-                if VERBOSE: print("Will delete %i of %i " % (N, len(what)), file=stderr)
-                if VERBOSE: print(what, file=stderr)
-                todel.extend( sample( what, N))
+    for int_len, key in zip(IVs, bins):
+        wanted_events = ceil(BackupAgeFreqDist[key]*int_len)
+        events        = hist[key]
+        if VERBOSE:
+            print("We have %i events (of %i) in slot: up to %i days (int len %i)" % (len(events), wanted_events, key, int_len))
+            pp.pprint(events)
+
+        if len(events) > wanted_events:
+            # we'll have to select N backups which shall be deleted
+            N = floor(len(events) - wanted_events)
+            if VERBOSE: print("Will delete %i of %i " % (N, len(events)), file=stderr)
+            todel.extend( sample( events, N))
+
     return todel
 
 ##############################################################################
@@ -102,6 +118,7 @@ def main():
     for backup_root in argv[1:]:
         if not path.exists(backup_root):
             if backup_root in [ "-v", "--verbose" ]:
+                global VERBOSE
                 VERBOSE=True
             else:
                 print("Skipping not existing directory %s", file=stderr)
